7ce76d0a772226a83a93919eb68d3e261cc614f8
Lakshyajit <lakshyajit@localhost.localdomain>
2019-05-23 19:21:35 +0530

    Updates

---
+++ precache-manifest.f6b9787b66d3ad75b2b2c16df7add81d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfca0c46324e2f44cf435575baf07b5",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "48152e8c77408869e591",
+    "url": "/weatherapp/static/css/main.0c1ddae7.chunk.css"
+  },
+  {
+    "revision": "3daffce866fd9cf2a656",
+    "url": "/weatherapp/static/js/2.9acc275a.chunk.js"
+  },
+  {
+    "revision": "48152e8c77408869e591",
+    "url": "/weatherapp/static/js/main.58209170.chunk.js"
+  },
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  },
+  {
+    "revision": "4a5f3bd28048e3228d5108bbc4414117",
+    "url": "/weatherapp/static/media/weather2.4a5f3bd2.jpg"
+  }
+]);
